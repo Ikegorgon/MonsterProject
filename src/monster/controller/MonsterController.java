e37c081694ea202a8f41fa941807c23979316ef0
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-21 13:15:10 -0600

    Converted to popup display and added protective code :)

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -10,35 +10,64 @@ public class MonsterController {
 		popup = new MonsterDisplay();
 	}
 	public void create() {
-		System.out.println("Create a Monster!");
+//		System.out.println("Create a Monster!");
+		popup.displayText("Create A Monster!");
 		
-		Scanner nameScanner = new Scanner(System.in);
-		System.out.println("What is your monsters name? (String)");
-		String name = nameScanner.nextLine();
+//		Scanner nameScanner = new Scanner(System.in);
+//		System.out.println("What is your monsters name? (String)");
+		String nameS = popup.getResponse("What is your monsters name? (String)");
+//		String name = nameScanner.nextLine();
+		String name = nameS;
 		
-		Scanner bellyScanner = new Scanner(System.in);
-		System.out.println("True/False: Your monster has a belly button? (true or false)");
-		boolean belly = bellyScanner.nextBoolean();
+//		Scanner bellyScanner = new Scanner(System.in);
+//		System.out.println("True/False: Your monster has a belly button? (true or false)");
+		boolean belly = false;
+		String bellyS = popup.getResponse("True/False: Your monster has a belly button? (true or false)");
+//		boolean belly = bellyScanner.nextBoolean();
+		if (isValidBoolean(bellyS)) {
+			belly = Boolean.parseBoolean(bellyS);
+		}
 		
-		Scanner legScanner = new Scanner(System.in);
-		System.out.println("How many legs does your monster have? (Number)");
-		double legs = legScanner.nextDouble();
+//		Scanner legScanner = new Scanner(System.in);
+//		System.out.println("How many legs does your monster have? (Number)");
+		double legs = 0;
+		String legsS = popup.getResponse("How many legs does your monster have? (Number)");
+//		double legs = legScanner.nextDouble();
+		if (isValidDouble(legsS)) {
+			legs = Double.parseDouble(legsS);
+		}
 		
-		Scanner nosesScanner = new Scanner(System.in);
-		System.out.println("How many noses does your monster have? (Number)");
-		int noses = nosesScanner.nextInt();
+//		Scanner nosesScanner = new Scanner(System.in);
+//		System.out.println("How many noses does your monster have? (Number)");
+		int noses = 0;
+		String nosesS = popup.getResponse("How many noses does your monster have? (Number)");
+//		int noses = nosesScanner.nextInt();
+		if (isValidInteger(nosesS)) {
+			noses = Integer.parseInt(nosesS);
+		}
 		
-		Scanner eyesScanner = new Scanner(System.in);
-		System.out.println("How many eyes does your monster have? (Number)");
-		int eyes = eyesScanner.nextInt();
+//		Scanner eyesScanner = new Scanner(System.in);
+//		System.out.println("How many eyes does your monster have? (Number)");
+		int eyes = 0;
+		String eyesS = popup.getResponse("How many eyes does your monster have? (Number)");
+//		int eyes = eyesScanner.nextInt();
+		if (isValidInteger(eyesS)) {
+			eyes = Integer.parseInt(eyesS);
+		}
 		
-		Scanner armsScanner = new Scanner(System.in);
-		System.out.println("How many arms does your monster have? (Number)");
-		int arms = armsScanner.nextInt();
+//		Scanner armsScanner = new Scanner(System.in);
+//		System.out.println("How many arms does your monster have? (Number)");
+		int arms = 0;
+		String armsS = popup.getResponse("How many arms does your monster have? (Number)");
+//		int arms = armsScanner.nextInt();
+		if (isValidInteger(armsS)) {
+			arms = Integer.parseInt(armsS);
+		}
 		
 		Monster myMonster = new Monster(name, belly, legs, noses, eyes, arms);
 		
-		System.out.println(myMonster);
+//		System.out.println(myMonster);
+		popup.displayText(myMonster.toString());
 //		nameScanner.close();
 //		bellyScanner.close();
 //		legScanner.close();
@@ -47,19 +76,51 @@ public class MonsterController {
 //		armsScanner.close();
 	}
 	public void start() {
-		Monster monster = new Monster();
-		Monster newMonster = new Monster("Juble", false, 4, 7, 1, 34);
-		System.out.println(monster);
-		System.out.println(newMonster);
-		System.out.println("Juble was attacked and lost a few arms.");
-		newMonster.setarm(newMonster.getarm() - 3);
-		System.out.println(newMonster);
-		popup.displayText("Ready to play?");
-		String answer = popup.getResponse("How's it going?");
-		System.out.println(answer);
+//		Monster monster = new Monster();
+//		Monster newMonster = new Monster("Juble", false, 4, 7, 1, 34);
+//		popup.displayText(monster.toString());
+//		System.out.println(monster);
+//		System.out.println(newMonster);
+//		System.out.println("Juble was attacked and lost a few arms.");
+//		newMonster.setarm(newMonster.getarm() - 3);
+//		System.out.println(newMonster);
+//		popup.displayText("Ready to play?");
+//		String answer = popup.getResponse("How's it going?");
+//		System.out.println(answer);
 		boolean CreateMonster = true;
 		while (CreateMonster) {
 			create();
+			 
 		}
+	}
+	private boolean isValidInteger(String sample) {
+		boolean valid = false;
+		try {
+			Integer.parseInt(sample);
+			valid = true;
+		} catch (NumberFormatException error) {
+			popup.displayText("You need to enter a valid integer. " + sample + " is not a valid integer.");
+		}
+		return valid;
+	}
+	private boolean isValidDouble(String sampleDouble) {
+		boolean valid = false;
+		try {
+			Double.parseDouble(sampleDouble);
+			valid = true;
+		} catch (NumberFormatException error) {
+			popup.displayText("You need to enter a valid double. " + sampleDouble + " is not a valid double.");
+		}
+		return valid;
+	}
+	private boolean isValidBoolean(String sampleBoolean) {
+		boolean valid = false;
+		try {
+			Boolean.parseBoolean(sampleBoolean);
+			valid = true;
+		} catch (NumberFormatException error) {
+			popup.displayText("You need to enter a valid boolean(true/false) " + sampleBoolean + " is not a valid boolean.");
+		}
+		return valid;
 	}
 }
