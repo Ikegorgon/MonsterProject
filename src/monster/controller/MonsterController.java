741b40b2dd87d37829c0390ceaa138a9d3d283a6
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-26 13:17:24 -0600

    changed if to while statements and added ask class

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -24,8 +24,9 @@ public class MonsterController {
 		boolean belly = false;
 		String bellyS = popup.getResponse("True/False: Your monster has a belly button? (true or false)");
 //		boolean belly = bellyScanner.nextBoolean();
-		if (isValidBoolean(bellyS)) {
+		while (isValidBoolean(bellyS)) {
 			belly = Boolean.parseBoolean(bellyS);
+			break;
 		}
 		
 //		Scanner legScanner = new Scanner(System.in);
@@ -33,8 +34,9 @@ public class MonsterController {
 		double legs = 0;
 		String legsS = popup.getResponse("How many legs does your monster have? (Number)");
 //		double legs = legScanner.nextDouble();
-		if (isValidDouble(legsS)) {
+		while (isValidDouble(legsS)) {
 			legs = Double.parseDouble(legsS);
+			break;
 		}
 		
 //		Scanner nosesScanner = new Scanner(System.in);
@@ -42,8 +44,9 @@ public class MonsterController {
 		int noses = 0;
 		String nosesS = popup.getResponse("How many noses does your monster have? (Number)");
 //		int noses = nosesScanner.nextInt();
-		if (isValidInteger(nosesS)) {
+		while (isValidInteger(nosesS)) {
 			noses = Integer.parseInt(nosesS);
+			break;
 		}
 		
 //		Scanner eyesScanner = new Scanner(System.in);
@@ -51,8 +54,9 @@ public class MonsterController {
 		int eyes = 0;
 		String eyesS = popup.getResponse("How many eyes does your monster have? (Number)");
 //		int eyes = eyesScanner.nextInt();
-		if (isValidInteger(eyesS)) {
+		while (isValidInteger(eyesS)) {
 			eyes = Integer.parseInt(eyesS);
+			break;
 		}
 		
 //		Scanner armsScanner = new Scanner(System.in);
@@ -60,8 +64,9 @@ public class MonsterController {
 		int arms = 0;
 		String armsS = popup.getResponse("How many arms does your monster have? (Number)");
 //		int arms = armsScanner.nextInt();
-		if (isValidInteger(armsS)) {
+		while (isValidInteger(armsS)) {
 			arms = Integer.parseInt(armsS);
+			break;
 		}
 		
 		Monster myMonster = new Monster(name, belly, legs, noses, eyes, arms);
@@ -87,11 +92,21 @@ public class MonsterController {
 //		popup.displayText("Ready to play?");
 //		String answer = popup.getResponse("How's it going?");
 //		System.out.println(answer);
-		boolean CreateMonster = true;
-		while (CreateMonster) {
+		while (ask()) {
 			create();
-			 
 		}
+	}
+	public boolean ask () {
+		boolean valid = false;
+		String ans = popup.getResponse("Want to make a monster? (yes/no)");
+		if (ans.toLowerCase().equals("yes") || ans.toLowerCase().equals("y")) {
+			valid = true;
+		} else if (ans.toLowerCase().equals("no") || ans.toLowerCase().equals("n")){
+			valid = false;
+		} else {
+			ask();
+		}
+		return valid;
 	}
 	private boolean isValidInteger(String sample) {
 		boolean valid = false;
